6bbace9d3da88a3eacfbe6e5a83f448bcc84e908
Sean Winnik <winnik.sean@gmail.com>
2023-08-21 16:49:16 -0400

    data directory created to hold works, work component sketched out

---
+++ src/data/work.js
@@ -0,0 +1,53 @@
+const workCollection = [
+  {
+    title: "I'm Still Here",
+    publication: "Michigan Quarterly Review Mixtape",
+    date: "February 2023",
+    content:
+      "\"Suicide slumps in the passenger seat, checking his watch. 'Are we there yet?' Suicide asks, and I say: 'Not even close.' He just smirks at me with his spatulate jaw, an icy crackle in his eyes. Though he isn’t what he used to be, his white shirt, as always, is immaculate.\"",
+  },
+  {
+    title: "Evidence",
+    publication: "Gulf Coast Journal",
+    date: "February 2023",
+    content:
+      '"With their hands clasped and knuckles squeezed white, the newlyweds descended the church steps. Flashing cameras froze the tossed rice in midair. Later that day at the crime scene, when the detective was handed the photos he could hear the grains clatter to the pavement."',
+  },
+  {
+    title: "Burning",
+    publication: "Narrative Magazine",
+    date: "January 2023",
+    content:
+      '"He touched down in LA, splashed water on his face in the airport bathroom, and stood in line at the Budget car rental place. He got the key and wandered out into the lot and thought about how, one day, when everyone had been killed or whatever, these cars would still be here, empty, accruing dust, their tires oozing into the concrete."',
+  },
+  {
+    title: "Last Stop",
+    publication: "Bellevue Literary Review",
+    date: "Fall 2022",
+    content:
+      '"All of us are bound for the same place: the shining city on the hill, or the grave, or just the next in an endless series of identical platforms full of identical people, stepping in and out of our collective stillborn dream."',
+  },
+  {
+    title: "Good Neighbors",
+    publication: "Narrative Magazine",
+    date: "April 2022",
+    content:
+      '"I couldn’t make out what they were saying—it was the anger I heard, the volcanic flow of it from the man. His voice bubbled like lava. It crashed and struck."',
+  },
+  {
+    title: "A Last Set",
+    publication: "Narrative Magazine",
+    date: "August 2021",
+    content:
+      '"Paul was moving to Sacramento after the tour. Which made this our farewell tour, even if we didn’t have that many fans to say farewell to."',
+  },
+  {
+    title: "Making a Difference",
+    publication: "Narrative Magazine",
+    date: "August 2019",
+    content:
+      '"I wasn’t a hoarder per se, but I did have trouble letting go of objects because it was like letting go of a dream you once had of yourself."',
+  },
+];
+
+export default workCollection;
