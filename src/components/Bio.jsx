6089922a785e2140a394c53e3c1c1ddd72cab769
Sean Winnik <winnik.sean@gmail.com>
2023-11-02 11:22:24 -0700

    storing profile in assets

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import profile from "../assets/images/profile.jpg";
 
 const Bio = () => {
   return (
@@ -13,10 +14,7 @@ const Bio = () => {
           }}
         >
           <div style={styles.h1}> Daniel Pope</div>
-          <img
-            src="https://media.licdn.com/dms/image/D4E03AQFM-oPf4jIItw/profile-displayphoto-shrink_800_800/0/1676838037708?e=1704326400&v=beta&t=7Yq4UI6GLeGAi4DOnZntGMaahkVmMW-jqdYfJW1t_5w"
-            style={styles.profileImage}
-          />
+          <img src={profile} style={styles.profileImage} />
         </div>
         <div style={styles.p}>
           {" "}
